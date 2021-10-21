9a073a98c828c1d823392b9b738b1eaa944ee112
ViktoriaMolochii <victoriamolochiy@gmail.com>
2021-10-21 20:33:43 +0300

    create base gulp project

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+function Person(name, sname, age){
+    this.firstName = name;
+    this.secondName = sname;
+    this.age = age;
+}
+const newPerson = new Person('Ivan', 'Ivanov', 25);
+console.log('newPerson', newPerson);
